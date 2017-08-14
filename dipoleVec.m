712421680cb52fd6a42d18d865620f8d3567828b
cndnb <blinnc@uw.edu>
2017-08-14 10:04:43 -0700

    Adds ols functionality
    Seems to be working, but the amplitude of the fit wave is different every compile.

--- dipoleVec.m
+++ dipoleVec.m
@@ -1,8 +1,8 @@
 %Constructs a time dependent Seattle dipole vector
 function ret = dipoleVec(t, seattleLat, seattleLong, compassDir, dM)
-  omegaEarth = 2*pi*(1/86164.0916);
-  A = tones(length(t),1).*dM*cos(compassDir)*cos(seattleLong);
-  B = dM*sin(compassDir)*sin(seattleLat + omegaEarth.*t);
-  C = dM*sin(compassDir)*cos(seattleLong+ omegaEarth.*t);
+  oE = 2*pi*(1/86164.0916);
+  A = ones(length(t),1).*dM*cos(compassDir)*cos(seattleLong);
+  B = dM*sin(compassDir)*sin(seattleLat + oE.*t);
+  C = dM*sin(compassDir)*cos(seattleLong+ oE.*t);
   ret = [t, A, B, C];
 endfunction
--- fakeSignalTest.m
+++ fakeSignalTest.m
@@ -9,9 +9,11 @@ A=0;
 B=1;
 C=1;
 Amp=10;
-omegaComp = 2*pi*.01;
 
-t=1:86165/5;%31536000; 
+omegaComp = 2*pi*.01;
+omegaEarth = 2*pi*(1/86164.0916);
+
+t=1:floor(86165/10);%31536000; 
 t= t';
 dV = [];
 for i=1:length(t)
@@ -24,8 +26,25 @@ endfor
 %Constructs matrix for plotting
 out = [];
 for k=1:length(t)
-out = [out; k,dot(dV(k,2:4),fS(k,2:4))];
+out = [out; k,dot(dV(k,2:4),fS(k,2:4))+randn(1)*50];
 endfor
+%Creates design matrix for ols fit
+X = ones(length(t),6);
+X(:,1)= sin(omegaComp*t).*sin(omegaEarth*t);
+X(:,2)= sin(omegaComp*t).*cos(omegaEarth*t);
+X(:,3)= cos(omegaComp*t).*sin(omegaEarth*t);
+X(:,4)= cos(omegaComp*t).*cos(omegaEarth*t);
+X(:,5)= sin(omegaComp*t);
+X(:,6)= cos(omegaComp*t);
+%Ols output
+[beta, sigma, r, err, cov] = ols2(out(:,2),X);
 %Plot!
+figure(1);
+
+%Original data
 plot(out(:,1),out(:,2));
+
+figure(2);
+%Linear fit
+plot(t,X*beta);
 
